007425438e05795b0fcd8080e96c7d9c3b4fd675
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-04-19 20:26:34 +0500

    Call ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 document.getElementById('call-inline-query').addEventListener('click', () => {
   window.Telegram.WebApp.switchInlineQuery('Test query', ['users']);
-})
+});
+
+window.Telegram.WebApp.ready();
