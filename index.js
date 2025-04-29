aeb8b9d6c657fa924dbd7342926c99415bd7b4a3
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-04-29 20:34:37 +0500

    Updates

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var { postEvent, setDebug, targetOrigin } = window.tapps.bridge;
+var { postEvent, setDebug, targetOrigin, on } = window.tapps.bridge;
 
 targetOrigin.set('*');
 setDebug(true);
@@ -8,6 +8,14 @@ document.getElementById('call-inline-query').addEventListener('click', () => {
     query: 'Test query',
     chat_types: ['users']
   });
+});
+
+document.getElementById('call-show-storage').addEventListener('click', () => {
+  postEvent('web_app_secure_storage_get_key', { key: 'TEST' });
+})
+
+on('secure_storage_key_received', res => {
+  alert('Received storage key' + JSON.stringify(res));
 });
 
 postEvent('web_app_ready');
