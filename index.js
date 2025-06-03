79f447937464adc820f763c15082426e591a0c99
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-06-04 00:22:52 +0500

    Updates

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ document.getElementById('call-secure-storage-set').addEventListener('click', () 
   alert('Called web_app_secure_storage_save_key with { key: "AAA", value: "BBB" }');
 });
 
+document.getElementById('go-to-input-test').addEventListener('click', () => {
+  document.getElementById('main').style.display = 'none';
+  document.getElementById('input-test').style.display = '';
+  postEvent('web_app_setup_main_button', { is_visible: false });
+  postEvent('web_app_setup_secondary_button', { is_visible: false });
+});
+
 const input = document.getElementById('multiple-files-input');
 input.addEventListener('change', e => {
   alert(`${input.files.length} files were selected`);
