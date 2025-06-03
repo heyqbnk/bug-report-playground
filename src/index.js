66cfc96374b9ee52f11780bdedaeefe73402bf0c
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-06-04 01:18:00 +0500

    Some changes

--- src/index.js
+++ src/index.js
@@ -16,6 +16,19 @@ document.getElementById('call-secure-storage-set').addEventListener('click', () 
     key: 'AAA',
     value: 'BBB',
   });
+  alert('Called web_app_secure_storage_save_key with { key: "AAA", value: "BBB" }');
+});
+
+document.getElementById('go-to-input-test').addEventListener('click', () => {
+  document.getElementById('main').style.display = 'none';
+  document.getElementById('input-test').style.display = '';
+  postEvent('web_app_setup_main_button', { is_visible: false });
+  postEvent('web_app_setup_secondary_button', { is_visible: false });
+});
+
+const input = document.getElementById('multiple-files-input');
+input.addEventListener('change', e => {
+  alert(`${input.files.length} files were selected`);
 })
 
 on('secure_storage_key_received', res => {
